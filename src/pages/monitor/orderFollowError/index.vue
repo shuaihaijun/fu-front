dbc8b098b5a30ff34622568ef72e687a0c32e10c
shuaige <haijun13811581906@163.com>
2020-04-28 15:51:34 +0800

    bug修改

--- src/pages/monitor/orderFollowError/index.vue
+++ src/pages/monitor/orderFollowError/index.vue
@@ -194,6 +194,8 @@
             prop: 'orderCloseDate',
             label: '平仓时间',
             width: '',
+            dateFormat: true,
+            format: 'yyyy-MM-dd HH:mm:ss',
             align: 'center'
           }
         ],
